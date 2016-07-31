df057d34166d6595af4e3a1fe5adb8e219999c5b
MikeLeeMcLau <mlmclaughlin@gmail.com>
2016-07-31 11:51:29 +0200

    Week 3 Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function will cache the steps required to find the inverse the the mean of matrix.
+## 
 
-## Write a short comment describing this function
+## Get a matrix and calculate the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+
+     
+    invx <- NULL  ##Set inverse to null
+    
+    set <- function(y) {  #Build matrix
+             y <<- x
+             invx <<- NULL
+    }
+    ##get values of matrix
+    get <- function()  x
+    setInverseMatrix <- function(inverse) invx <<- inverse
+    getInverseMatrix <- function() invx
+    
+    ## save to the current working environment
+            list(set = set, get = get,
+             setInverseMatrix = setInverseMatrix,
+             getInverseMatrix = getInverseMatrix)
 
 }
 
 
-## Write a short comment describing this function
+## Checks to see if x is avaiable and returns the inverse.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## Try to get the data stored i ncache (inverse of the matrix).
+        invx <- x$getInverseMatrix()
+
+        # Inverted matrix otherwise create a matrix
+        if (!is.null(invx)) {
+                # display matrix on screen
+                return(invx)
+        }
+
+        # create matrix
+        invx <- x$get()
+
+                final_matrix = {
+                # set inverted matrix in cache
+                x$setInverseMatrix(invx)
+                invx <- solve(invx)
+        } 
+
+        # display matrix 
+        {print(invx)}
 }
